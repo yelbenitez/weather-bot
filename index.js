469134bbe5c70b83fbe0bf5c05c42dfd30d66a32
Yel <yelbenitez5@gmail.com>
2018-11-29 14:47:55 -0800

    celsius unit

--- index.js
+++ index.js
@@ -48,7 +48,7 @@ try{
                                         let clouds = result.clouds['all'];
                                         let pressure = result.main['pressure'];
 
-                                        let output = "today in "+ location +": "+ weather + ", temperature from " + Math.round(temp_min)  + "°C to " + Math.round(temp_max) + "°C , wind " + wind + "m/s. clouds " + clouds + "% " + pressure+" hpa";
+                                        let output = "today in "+ location +": "+ weather + ", temperature from " + Math.round(temp_min)  + " °C to " + Math.round(temp_max) + " °C , wind " + wind + "m/s. clouds " + clouds + "% " + pressure+" hpa";
 
                                           response.setHeader('Content-Type', 'application/json', 'charset=utf-8');
                                           var pass = {
